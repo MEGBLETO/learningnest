1af818a2a738c1f9f1403f9cee4c54f24afe9d5a
carnel MEGBLETO <carnelmegbleto@carnels-MacBook-Pro.local>
2022-01-01 22:48:06 +0100

    almost there

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,8 +1,28 @@
-import {Controller} from '@nestjs/common'
+import {Controller, Get, Param, Post, Body, Put} from '@nestjs/common'
 
 
 
 @Controller('students')
-class StudentController{
+export class StudentController{
+   @Get()
+    getStudents(){
+        return "All the students"
+    }
+
+    @Get('/:id')
+    getStudentdsbyId(
+        @Param() params: {id: string}
+    ){
+        //console.log(params.id)
+        return "i'am a single student id"
+    }
+
+    @Post('')
+    Addastudent(
+        @Param() params: {id: string}
+    ){
+        //console.log(params.id)
+        return "i'am a newly added student"
+    }
 
 }
